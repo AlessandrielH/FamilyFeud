d04dfc05cbd09e6f2f6e0281a1e9a1d13f3ecd0c
AlessandrielH <65774205+AlessandrielH@users.noreply.github.com>
2021-07-26 19:01:08 -0400

    Update Game.cpp (#33)
    separated implementation file for Game

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,6 @@
-#include "Cards.cpp"
-#include "Team.cpp"
+#include "Game.h"
+#include "Cards.h"
+#include "Team.h"
 
 #include <map>
 #include <string>
@@ -9,24 +10,7 @@
 
 using namespace std;
 
-class Game
-{
-
-private:
-	//initialize Cards Object
-		Cards cards;
-
-public:
-	//Game();
-	//~Game();
-	//void startPlaying(); 
-	//void displayInstructions();
-	//void displayMenu();
-	//void createCards();
-	
-	
-
-	void startPlaying()
+	void Game::startPlaying()
 	{
 		int menu;
 
@@ -49,8 +33,8 @@ public:
 				cout << "Enter team 2's name: ";
 				cin >> team2;
 				cout << "Hello " << team2 << ". " << endl;
-				
-				points[team1] =  0;
+
+				points[team1] = 0;
 				points[team2] = 0;
 
 				for (int round = 0; round < cards.totalQuestions(); round++)
@@ -58,7 +42,7 @@ public:
 					cout << "ROUND:" << round + 1 << endl;
 					int wrong = 0;
 					int correct = 0;
-					do{
+					do {
 						cout << "There are " << cards.totalAnswers(round) << " possible answers." << endl;
 						cards.getQuestion(round);
 						cin >> answer;
@@ -73,15 +57,15 @@ public:
 						{
 							wrong++;
 							cout << "Your answer is incorrect. " << endl;
-							cout<<"You have been incorrect "<<wrong<<" times."<<endl;
+							cout << "You have been incorrect " << wrong << " times." << endl;
 						}
-						
-						if (wrong==3)
+
+						if (wrong == 3)
 						{
 							//The other team answers question
 						}
 					} while (wrong < 3 && correct < cards.totalAnswers(round));
-					/*repeats question until team guesses all answers 
+					/*repeats question until team guesses all answers
 					or answers incorrectly 3 times*/
 				}
 			}
@@ -107,38 +91,38 @@ public:
 
 	}
 
-	void createCards(){
+	void Game::createCards() {
 		int numCard;
 		int numAns;
-		int numPoint; 
-		string sentence; 
-		string ans; 
+		int numPoint;
+		string sentence;
+		string ans;
 
 		vector<int> inpoints;
-		vector<string> inanswr; 
-		
+		vector<string> inanswr;
+
 		cout << "How many cards do you want to create? " << endl;
-		cin >> numCard; 
+		cin >> numCard;
 
 
-		for(int i = 0; i < numCard; i++){
+		for (int i = 0; i < numCard; i++) {
 			do {
 				cout << "Enter question number " << i + 1 << endl;
 				cin >> sentence;
-			} while (cards.verifyCard(sentence)==true);
+			} while (cards.verifyCard(sentence) == true);
 			cards.setQuestion(sentence);
-			
-			cout << "How many possible answers do you want to add for this question " << endl; 
+
+			cout << "How many possible answers do you want to add for this question " << endl;
 			cin >> numAns;
-			cout << "Enter " << numAns << " possible answers below" << endl; 
-			for(int i = 0; i < numAns; i++){
+			cout << "Enter " << numAns << " possible answers below" << endl;
+			for (int i = 0; i < numAns; i++) {
 				cin >> ans;
 				inanswr.push_back(ans);
 			}
 			cards.setAnswers(inanswr);
-			
+
 			cout << "Assign " << numAns << " point for each answers below" << endl;
-			for(int i = 0; i < numAns; i++){
+			for (int i = 0; i < numAns; i++) {
 				cin >> numPoint;
 				inpoints.push_back(numPoint);
 			}
@@ -147,40 +131,40 @@ public:
 		}
 
 	}
-	
-	void displayInstructions()
+
+	void Game:: displayInstructions()
 	{
-		cout<< "INSTRUCTIONS\n\n";
-    		
-		cout<<"Play Game Instructions:"<<endl;
-		cout<< "1. You will be presented with the number of possible answers.\n";
-		cout<<"2. The question will be displayed. \n";
-		cout<<"3. The team called will enter an answer to the question.\n";
-		cout<<"4. Possible Responses:\n";
-		cout<<"\t ~  If the team answers correctly, that team will receive the points\n";
-		cout<<"\t for that answer and enter in a new guess.\n";
-		cout<<"\t ~  If the answer is wrong, that team will get 2 more chances to\n";
-		cout<<"\t correctly answer the question.\n";
-		cout<<"\t ~  If the answer, is still wrong after 2 chances, the other team\n";
-		cout<<"\t gets 1 chance to guess the answer.\n";
-		cout<<"\t ~  If the other team guesses correctly, they receive all of the points\n";
-		cout<<"\t from that round.\n";
-		cout<<"\t ~  If the other team guesses incorrectly, the original team keeps all\n";
-		cout<<"\t of their points.\n";
-		cout<<"5. The players will be asked if they want to play another round.\n";
-		cout<<"\t ~  If they answer yes, the points will be carried over to the next\n";
-		cout<<"\t round.\n";
-		cout<<"\t ~  If they answer no, the game will return to the Menu.\n"<<endl;
-		
-		cout<<"Add Card Instructions:"<<endl;
-	    	cout<<"In this section, You will add questions to the game."<<endl;
-	    	cout<<"1. Create a question.\n";
-	    	cout<<"2. Add 4-8 possible answers\n";
-	    	cout<<"3. Assign points to each answer\n";
-	    	cout<<"***The total points for each question should be 100\n"<<endl;
+		cout << "INSTRUCTIONS\n\n";
+
+		cout << "Play Game Instructions:" << endl;
+		cout << "1. You will be presented with the number of possible answers.\n";
+		cout << "2. The question will be displayed. \n";
+		cout << "3. The team called will enter an answer to the question.\n";
+		cout << "4. Possible Responses:\n";
+		cout << "\t ~  If the team answers correctly, that team will receive the points\n";
+		cout << "\t for that answer and enter in a new guess.\n";
+		cout << "\t ~  If the answer is wrong, that team will get 2 more chances to\n";
+		cout << "\t correctly answer the question.\n";
+		cout << "\t ~  If the answer, is still wrong after 2 chances, the other team\n";
+		cout << "\t gets 1 chance to guess the answer.\n";
+		cout << "\t ~  If the other team guesses correctly, they receive all of the points\n";
+		cout << "\t from that round.\n";
+		cout << "\t ~  If the other team guesses incorrectly, the original team keeps all\n";
+		cout << "\t of their points.\n";
+		cout << "5. The players will be asked if they want to play another round.\n";
+		cout << "\t ~  If they answer yes, the points will be carried over to the next\n";
+		cout << "\t round.\n";
+		cout << "\t ~  If they answer no, the game will return to the Menu.\n" << endl;
+
+		cout << "Add Card Instructions:" << endl;
+		cout << "In this section, You will add questions to the game." << endl;
+		cout << "1. Create a question.\n";
+		cout << "2. Add 4-8 possible answers\n";
+		cout << "3. Assign points to each answer\n";
+		cout << "***The total points for each question should be 100\n" << endl;
 	}
 
-	void displayMenu()
+	void Game::displayMenu()
 	{
 		cout << "****************************************" << endl;
 		cout << "*           Family Feud Game           *" << endl;
@@ -194,4 +178,3 @@ public:
 		cout << "* 4. Exit game.                        *" << endl;
 		cout << "****************************************" << endl;
 	}
-};
