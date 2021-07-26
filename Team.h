41e53dc5de2e672ed1274c36ee987552f0a00520
AlessandrielH <65774205+AlessandrielH@users.noreply.github.com>
2021-07-26 18:04:45 -0500

    Create Team.h
    added Team header file

---
+++ Team.h
@@ -0,0 +1,20 @@
+#define TEAM
+#include <string>
+#include <map>
+using namespace std;
+
+class Team
+{
+private:
+	string name;
+	map<string, int> points;
+
+
+public:
+	Team();
+	~Team();
+	string getName();
+	void setName(string);
+	void addPoints(int);
+};
+#endif
