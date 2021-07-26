2901fead5cc377ed72c10c9b0ee15ddcf56c7d37
AlessandrielH <65774205+AlessandrielH@users.noreply.github.com>
2021-07-26 18:59:09 -0400

    Update FamilyFeud.cpp (#32)
    changed include to Game.h

--- FamilyFeud.cpp
+++ FamilyFeud.cpp
@@ -1,6 +1,6 @@
 // FamilyFeud.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-#include "Game.cpp"
+#include "Game.h"
 #include <iostream>
 
 using namespace std;
